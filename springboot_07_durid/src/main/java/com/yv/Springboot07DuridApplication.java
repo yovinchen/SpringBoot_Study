19ab07f8cec4ee4d7e49bf3aad0a37b6d01888c0
Yo Vinchen <2982554722@qq.com>
2023-03-15 12:33:09 +0800

    Initial commit

---
+++ springboot_07_durid/src/main/java/com/yv/Springboot07DuridApplication.java
@@ -0,0 +1,13 @@
+package com.yv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot07DuridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot07DuridApplication.class, args);
+	}
+
+}
