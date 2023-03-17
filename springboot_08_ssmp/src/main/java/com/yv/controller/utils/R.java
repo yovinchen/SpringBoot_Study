113b034afb49e076f610219b2b103b7b2cb6b9d2
Yo Vinchen <2982554722@qq.com>
2023-03-17 22:36:45 +0800

    Initial commit

---
+++ springboot_08_ssmp/src/main/java/com/yv/controller/utils/R.java
@@ -0,0 +1,13 @@
+package com.yv.controller.utils;
+
+import lombok.Data;
+
+/**
+ * @author YoVinchen
+ * @date 2023/3/17 下午 8:49
+ */
+@Data
+public class R {
+    private Boolean flag;
+    private Object data;
+}
